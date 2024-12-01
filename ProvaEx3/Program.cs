0fc535078ca73c3595c998b08d63d1309eb1b2ca
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-12-01 18:13:53 +0100

    Closes #1 - prova 1

--- ProvaEx3/Program.cs
+++ ProvaEx3/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ProvaEx3
+{
+    public class Programa
+    {
+        // mètode per comprovar si el número és parell o no
+        public static bool IsEven(int numero)
+        {
+            return numero % 2 == 0;
+        }
+
+        public static void Main()
+        {
+            //constants
+            const string Msg = "Introdueix un número:";
+            //inici programa
+            Console.WriteLine(Msg);
+            int numero = Convert.ToInt32(Console.ReadLine());
+            // comprovem si és parell o senar utilitzant el mètode IsEven
+            if (IsEven(numero))
+            {
+                Console.WriteLine($"El número {numero} és parell.");
+            }
+            else
+            {
+                Console.WriteLine($"El número {numero} és senar.");
+            }
+        }
+    }
+}
